fec023c8d611ff7789a3b69575da05ea9d4eb2a7
w01fgang <sumin@unix-center.ru>
2020-09-13 18:11:07 +0200

    clarify execFile definition

--- tests/node_tests/child_process/execFile.js
+++ tests/node_tests/child_process/execFile.js
@@ -6,7 +6,7 @@ var execFile = require('child_process').execFile;
 execFile('ls', ['-lh']);
 
 // callback only.
-execFile('ls', function(error, stdout, stderr) {
+execFile('ls', function(error, stdout: string, stderr: string) {
   console.info(stdout);
 });
 
@@ -14,7 +14,7 @@ execFile('ls', function(error, stdout, stderr) {
 execFile('wc', {timeout: 250});
 
 // args + callback.
-execFile('ls', ['-l'], function(error, stdout, stderr) {
+execFile('ls', ['-l'], function(error, stdout: string, stderr: string) {
   console.info(stdout);
 });
 
@@ -22,6 +22,16 @@ execFile('ls', ['-l'], function(error, stdout, stderr) {
 execFile('ls', ['-l'], {timeout: 250});
 
 // Put it all together.
-execFile('ls', ['-l'], {timeout: 250}, function(error, stdout, stderr) {
+execFile('ls', ['-l'], {timeout: 250}, function(error, stdout: string, stderr: string) {
+  console.info(stdout);
+});
+
+// Buffer options + callback.
+execFile('ls', {encoding: 'buffer'}, function(error, stdout: Buffer, stderr: Buffer) {
+  console.info(stdout);
+});
+
+// Buffer Put it all together.
+execFile('ls', ['-l'], {encoding: 'buffer'}, function(error, stdout: Buffer, stderr: Buffer) {
   console.info(stdout);
 });
